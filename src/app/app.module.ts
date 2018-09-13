31654f837b457aa3efb1f91375fc969d1c099de3
Thiago Luiz Parrillo Rizzo <thiagorizzo@gmail.com>
2018-09-12 22:42:49 -0300

    adicionado componente listar-produtos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListarProdutosComponent } from './produto/listar-produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    ListarProdutosComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/produto/listar-produtos.component.ts
@@ -0,0 +1,19 @@
+// IMPORT
+import { Component } from '@angular/core';
+
+// DECORATOR: É uma função que recebe um objeto de configuração
+@Component({
+    selector: 'app-listar-produtos',
+    templateUrl: 'listar-produtos.compnent.html'
+})
+// CLASSE
+export class ListarProdutosComponent {
+    produtos : Array<any> = [
+        { codigo: 1, nome: 'World Of Warcraft', preco: 59.90 },
+        { codigo: 2, nome: 'Battlefield 1', preco: 79.00 },
+        { codigo: 3, nome: 'League Of Legends', preco: 0.00 },
+        { codigo: 4, nome: 'Destiny 2', preco: 150.00 }
+    ];
+
+    titulo : string = 'Lista de Produtos';
+}

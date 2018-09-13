4e7b91fa8e37fd55416ec3adbae81a0d2d188c21
Thiago Luiz Parrillo Rizzo <thiagorizzo@gmail.com>
2018-09-13 00:43:16 -0300

    adicionada tipagem forte através da interface IProduto; adicionados styles embutidos;

--- src/app/produto/listar-produtos.component.ts
+++ src/app/produto/listar-produtos.component.ts
@@ -1,19 +1,22 @@
 // IMPORT
 import { Component } from '@angular/core';
+import { IProduto } from './produto';
 
 // DECORATOR: É uma função que recebe um objeto de configuração
 @Component({
     selector: 'app-listar-produtos',
-    templateUrl: 'listar-produtos.component.html'
+    templateUrl: 'listar-produtos.component.html',
+    //styles: [ 'tbody td img { height: 100px; width: 100px; }' ],
+    styleUrls: [ 'listar-produtos.component.css' ]
 })
 // CLASSE
 export class ListarProdutosComponent {
     produtoSelecionado = undefined;
 
-    produtos : Array<any> = [
+    produtos : IProduto[] = [
         { codigo: 1, nome: 'World Of Warcraft', preco: 59.90, imagemUrl: 'WorldOfWarcraft.jpg', descricao: 'World of Warcraft é um jogo on-line, (MMORPG) da produtora Blizzard lançado em 2004. O jogo se passa no mundo fantástico de Azeroth.' },
         { codigo: 2, nome: 'Battlefield 1', preco: 79.00, imagemUrl: 'Battlefield1.jpg', descricao: 'Battlefield 1 é um jogo eletrônico de tiro em primeira pessoa ambientado na Primeira Guerra Mundial, desenvolvido pela EA DICE e publicada pela Electronic Arts.' },
-        { codigo: 3, nome: 'League Of Legends', preco: 0.00, descricao: 'League of Legends é um jogo online competitivo que mistura a velocidade e a intensidade de um RTS com elementos de RPG.' },
+        { codigo: 3, nome: 'League Of Legends', preco: 0.00, imagemUrl: undefined, descricao: 'League of Legends é um jogo online competitivo que mistura a velocidade e a intensidade de um RTS com elementos de RPG.' },
         { codigo: 4, nome: 'Destiny 2', preco: 150.00, imagemUrl: 'Destiny2.jpg', descricao: 'Dos criadores do aclamado jogo Destiny, chega a tão esperada sequência. Um shooter de ação que leva você a uma viagem épica pelo sistema solar.' }
     ];
 
---
+++ src/app/produto/produto.ts
@@ -0,0 +1,7 @@
+export interface IProduto {
+    codigo : number;
+    nome : string;
+    descricao : string;
+    preco : number;
+    imagemUrl : string;
+}

2f2817dc4f1042f1d5037d41bef686cb9a6e2f44
Thiago Luiz Parrillo Rizzo <thiagorizzo@gmail.com>
2018-09-12 22:55:14 -0300

    condição para apresentar ou não produtos cadastrados

--- src/app/produto/listar-produtos.component.ts
+++ src/app/produto/listar-produtos.component.ts
@@ -9,10 +9,10 @@ import { Component } from '@angular/core';
 // CLASSE
 export class ListarProdutosComponent {
     produtos : Array<any> = [
-        { codigo: 1, nome: 'World Of Warcraft', preco: 59.90 },
+        /*{ codigo: 1, nome: 'World Of Warcraft', preco: 59.90 },
         { codigo: 2, nome: 'Battlefield 1', preco: 79.00 },
         { codigo: 3, nome: 'League Of Legends', preco: 0.00 },
-        { codigo: 4, nome: 'Destiny 2', preco: 150.00 }
+        { codigo: 4, nome: 'Destiny 2', preco: 150.00 }*/
     ];
 
     titulo : string = 'Lista de Produtos';

13afa7ec1d9df612272523a865b171036f94bbef
Thiago Luiz Parrillo Rizzo <thiagorizzo@gmail.com>
2018-09-12 23:04:44 -0300

    lógica condicional movida do template para a classe do componente

--- src/app/produto/listar-produtos.component.ts
+++ src/app/produto/listar-produtos.component.ts
@@ -9,11 +9,14 @@ import { Component } from '@angular/core';
 // CLASSE
 export class ListarProdutosComponent {
     produtos : Array<any> = [
-        /*{ codigo: 1, nome: 'World Of Warcraft', preco: 59.90 },
+        { codigo: 1, nome: 'World Of Warcraft', preco: 59.90 },
         { codigo: 2, nome: 'Battlefield 1', preco: 79.00 },
         { codigo: 3, nome: 'League Of Legends', preco: 0.00 },
-        { codigo: 4, nome: 'Destiny 2', preco: 150.00 }*/
+        { codigo: 4, nome: 'Destiny 2', preco: 150.00 }
     ];
 
     titulo : string = 'Lista de Produtos';
+
+    possuiProdutos : () => boolean = 
+        () => { return this.produtos && this.produtos.length > 0 };
 }
